f272128b66af3c3b071778ebefbcfa3023a8e7da
TruongDang23 <quantruong050123@gmail.com>
2026-05-02 15:26:01 +0700

    change kernel and stride in stem. Remove stage 3, stage 4 in ConvNeXt

--- model/backbone.py
+++ model/backbone.py
@@ -10,7 +10,7 @@ from torchinfo import summary
 class LayerNorm2d(nn.Module):
     def __init__(self, num_channels):
         super().__init__()
-        self.norm = nn.LayerNorm(num_channels)
+        self.norm = nn.LayerNorm(num_channels, eps=1e-6)
 
     def forward(self, x):
         # x: (B, C, H, W) -> (B, H, W, C)
@@ -19,55 +19,116 @@ class LayerNorm2d(nn.Module):
         return x.permute(0, 3, 1, 2)
 
 
+def drop_path(x, drop_prob: float = 0., training: bool = False):
+    if drop_prob == 0. or not training:
+        return x
+    keep_prob = 1 - drop_prob
+    shape = (x.shape[0],) + (1,) * (x.ndim - 1)
+
+    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+    binary_tensor = torch.floor(random_tensor)
+
+    output = x.div(keep_prob) * binary_tensor
+    return output
+
+
+class DropPath(nn.Module):
+    def __init__(self, drop_prob=None):
+        super().__init__()
+        self.drop_prob = drop_prob
+    def forward(self, x):
+        return drop_path(x, self.drop_prob, self.training)
+    
+
+class Stem(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.norm = LayerNorm2d(out_channels)
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.norm(x)
+        return x
+    
+
+class DownSampling(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.norm = LayerNorm2d(dim)
+        self.conv = nn.Conv2d(dim, dim*2, kernel_size=2, stride=2)
+    def forward(self, x):
+        x = self.norm(x)
+        x = self.conv(x)
+        return x
+
 # =========================
 # ConvNeXt Block (simplified)
 # =========================
 
 class ConvNeXtBlock(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
         super().__init__()
         self.dwconv = nn.Conv2d(dim, dim, 7, padding=3, groups=dim)
         self.norm = LayerNorm2d(dim)
-        self.pwconv1 = nn.Conv2d(dim, 4 * dim, 1)
+        self.pwconv1 = nn.Linear(dim, 4 * dim)
         self.act = nn.GELU()
-        self.pwconv2 = nn.Conv2d(4 * dim, dim, 1)
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
         identity = x
         x = self.dwconv(x)
         x = self.norm(x)
+        x = x.permute(0, 2, 3, 1)
         x = self.pwconv1(x)
         x = self.act(x)
         x = self.pwconv2(x)
-        return x + identity
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2)
+        return identity + self.drop_path(x)
 
 
 # =========================
 # ConvNeXt Frontend (1/8)
 # =========================
 
+# Input: 640 x 480
 class ConvNeXtFrontend(nn.Module):
     def __init__(self):
         super().__init__()
+        self.stem = Stem(3, 96) # 320x240
 
-        self.stage1 = nn.Sequential(
-            nn.Conv2d(3, 64, 4, stride=2, padding=1),  # 320→160
-            ConvNeXtBlock(64)
-        )
+        self.stage1 = nn.ModuleList()
 
-        self.stage2 = nn.Sequential(
-            nn.Conv2d(64, 128, 2, stride=2),  # 160→80
-            ConvNeXtBlock(128)
-        )
+        for _ in range(3):
+            self.stage1.append(ConvNeXtBlock(96))
 
-        self.stage3 = nn.Sequential(
-            nn.Conv2d(128, 256, 2, stride=2),  # 80→40
-            ConvNeXtBlock(256)
-        )
+        self.downsample1 = DownSampling(96) #160x120
+
+        self.stage2 = nn.ModuleList()
+
+        for _ in range(3):
+            self.stage2.append(ConvNeXtBlock(192))
+        
+        self.downsample2 = DownSampling(192) # 80x60
+    
 
     def forward(self, x):
-        x = self.stage1(x)
-        x = self.stage2(x)
-        x = self.stage3(x)
-        return x  # (B, 256, 40, 80)
+        x = self.stem(x)
+        for block in self.stage1:
+            x = block(x)
+        x = self.downsample1(x)
+        for block in self.stage2:
+            x = block(x)
+        x = self.downsample2(x)
+        return x  
 
+
+device = torch.device("cpu")
+
+model = ConvNeXtFrontend().to(device)
+
+summary(model, input_size=(16, 3, 320, 640))
